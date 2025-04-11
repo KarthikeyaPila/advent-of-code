19cdd2c0ccf4e1a541348e41a51bee667829ece9
karthikeya <karthikeyapila3@gmail.com>
2025-04-11 18:48:18 +0530

    add helping library

--- lib/aoc_utils.h
+++ lib/aoc_utils.h
@@ -1,11 +1,6 @@
 #ifndef AOC_UTILS_H
 #define AOC_UTILS_H
-
-char *read_file_to_string(const char *filename);
-char **split_string(const char *input, const char *delim, int *count_out);
-int *convert_strings_to_ints(char **strs, int count);
-void free_string_array(char **arr, int count);
+char** read_lines_from_file(const char* filename, int* out_line_count);
 int* string_to_digit_array(char* str, int* out_len);
-int **parse_2d_int_grid(const char *input_str, int *rows, int *cols);
 
 #endif
